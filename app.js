a77600028e5e268736581e899bc752d162a44431
Celal Mete <87307659+CelalMete@users.noreply.github.com>
2025-12-16 17:56:58 +0300

    depolama zart zurtu

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 require("dotenv").config();
-const uploadİlan = require('./cloudinary');
+const uploadİlan = require('./public/scripts/clodinary');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
