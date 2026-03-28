75034afa3210ebc29b63025f2a387676c3bba684
Celal Mete <87307659+CelalMete@users.noreply.github.com>
2026-03-28 11:35:05 +0300

    aaaa

--- public/scripts/mesajlar.js
+++ public/scripts/mesajlar.js
@@ -7,14 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
             const cevap = await fetch('/getmessages'); 
             const veri = await cevap.json();
             veri.kisiler.forEach(element => {
+                const person=document.createElement('div')
                const namediv = document.createElement('div')
                const ppimg = document.createElement('img')
+               person.classList.add('personblock')
                namediv.innerText=element.atan.Ad;
                ppimg.src=element.atan.pp
                 namediv.classList.add('sender')
                 ppimg.classList.add('ppimg')
-                side.appendChild(ppimg)
-               side.appendChild(namediv)
+                side.appendChild(person)
+                person.appendChild(ppimg)
+               person.appendChild(namediv)
                 
             });
             console.log(veri)
